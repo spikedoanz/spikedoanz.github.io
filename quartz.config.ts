cb0336f8e7927a0f5c3bbcc2cbb48a1b892b2bfe
Mike Doan <spikedoanz@gmail.com>
2024-08-05 19:50:11 -0400

    gca

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,20 +23,30 @@ const config: QuartzConfig = {
           light: "#dce0e8",     // background
           lightgray: "#ccd0da", // dashed lines
           gray: "#eff1f5",
-          darkgray: "#7c7f93",
-          dark: "#4c4f69",
+          darkgray: "#7c7f93",  // regular text
+          dark: "#4c4f69",      // bold text
           secondary: "#f38ba8", // hyperlinks and site header
           tertiary: "#9ca0b0",  // cursor highlight
           highlight: "rgba(0, 0, 0, 0)",
         },
-        darkMode: {
-          light: "#11111b",
-          lightgray: "#6c7086",
-          gray: "#1e1e2e",
-          darkgray: "#b1b1b1",
-          dark: "#cdd6f4",
-          secondary: "#f38ba8",
-          tertiary: "#45475a",
+        // darkMode: { // catpuccin mocha
+        //   light: "#11111b",
+        //   lightgray: "#6c7086",
+        //   gray: "#1e1e2e",
+        //   darkgray: "#b1b1b1",
+        //   dark: "#cdd6f4",
+        //   secondary: "#f38ba8",
+        //   tertiary: "#45475a",
+        //   highlight: "rgba(0, 0, 0, 0)",
+        // },
+        darkMode: { // rosepine dawn 
+          light: "#faf4ed",
+          lightgray: "#cecacd",
+          gray: "#575279",
+          darkgray: "#575279",
+          dark: "#575279",
+          secondary: "#b4637a",
+          tertiary: "#dfdad9",
           highlight: "rgba(0, 0, 0, 0)",
         },
       },
