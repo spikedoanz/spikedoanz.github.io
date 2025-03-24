d2ff59393e6e910e8746a89f3d27615e6bd6d361
Mike Doan <spikedoanz@gmail.com>
2025-03-24 17:02:03 -0400

    wp

--- experiments/attnislogarithmic/font.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.font_manager
-
-# List all available fonts
-font_list = sorted([f.name for f in matplotlib.font_manager.fontManager.ttflist])
-for font in font_list:
-    print(font)
